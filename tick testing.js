5a13352a40e0320aea0906c1a80c47fe394aa204
OvOPlant <149712688+OvOPlant@users.noreply.github.com>
2024-01-12 08:05:11 -0500

    Create tick testing.js

---
+++ tick testing.js
@@ -0,0 +1,31 @@
+(function() {
+    let runtime = c3_runtimeInterface._GetLocalRuntime();
+    let notify = () => {};
+    
+    function Angle(){
+        runtime._iRuntime.objects.Collider.getFirstPickedInstance().angle = 90 * (Math.PI/180)
+    }
+
+    let testing = {
+        init() {
+            document.addEventListener("keydown", (event) => {
+                if (event.code === "KeyY") {
+                    if (event.shiftKey) {
+                        runtime._iRuntime.addEventListener("tick", () => Angle())
+                    }
+                }
+                if (event.code === "KeyU") {
+                    if (event.shiftKey) {
+                        runtime._iRuntime.removeEventListener("tick", Angle())
+                    }
+                }
+
+            })
+          
+            globalThis.ovoTesting = this;
+            notify("dude there is no notify command");
+        },
+      
+    };
+    testing.init();
+})();
