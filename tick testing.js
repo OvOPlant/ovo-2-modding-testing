643c4f6ed4e7353388481ee2faee0172348747a1
OvOPlant <149712688+OvOPlant@users.noreply.github.com>
2024-01-18 08:01:22 -0500

    Update tick testing.js

--- tick testing.js
+++ tick testing.js
@@ -3,7 +3,7 @@
     let notify = () => {};
     
     function Angle(){
-        runtime._iRuntime.objects.Collider.getFirstPickedInstance().angle = 90 * (Math.PI/180)
+        runtime._allObjectClasses[12]._instances[0]._iScriptInterface.angle = 90 * (Math.PI/180)
     }
 
     let testing = {
@@ -11,12 +11,12 @@
             document.addEventListener("keydown", (event) => {
                 if (event.code === "KeyY") {
                     if (event.shiftKey) {
-                        runtime._iRuntime.addEventListener("tick", () => Angle())
+                        runtime._iRuntime.addEventListener("tick", Angle)
                     }
                 }
                 if (event.code === "KeyU") {
                     if (event.shiftKey) {
-                        runtime._iRuntime.removeEventListener("tick", Angle())
+                        runtime._iRuntime.removeEventListener("tick", Angle)
                     }
                 }
 
